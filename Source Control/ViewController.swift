5aeafc6521d3ab9ccfd26a9a96bb676c8196a839
Keith Barnett <kgb@mmtuk.com>
2022-02-11 16:07:21 +0000

    Initial Commit

---
+++ Source Control/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Keith Barnett on 11/02/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
